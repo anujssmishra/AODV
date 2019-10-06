0201c1ad8be9903b1e99245d8a2ea45083e3a0b5
anujssmishra@gmail.com <anujssmishra@gmail.com>
2019-10-06 15:17:45 +0530

    Removed errors

--- Graph_create.c
+++ Graph_create.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <conio.h>
-#include <alloc.h>
+// #include <conio.h>
+#include <malloc.h>
 struct node
 {
 char vertex;
@@ -14,7 +14,7 @@ int main()
 {
 	 struct node *Adj[10];
 int i, no_of_nodes;
-clrscr();
+// clrscr();
 printf("\n Enter the number of nodes in G: ");
 scanf("%d", &no_of_nodes);
 	 for(i = 0; i < no_of_nodes; i++)
@@ -23,14 +23,14 @@ createGraph(Adj, no_of_nodes);
 printf("\n The graph is: ");
 displayGraph(Adj, no_of_nodes);
 deleteGraph(Adj, no_of_nodes);
-getch();
+// getch();
 	 return 0;
 }
 void createGraph(struct node *Adj[], int no_of_nodes)
 {
 struct node *new_node, *last;
 int i, j, n, val;
-	 for(i = 0; i < no_of_nodes; i++)	
+	 for(i = 0; i < no_of_nodes; i++)
 {
  last = NULL;
  printf("\n Enter the number of neighbours of %d: ", i);
@@ -40,13 +40,13 @@ int i, j, n, val;
  printf("\n Enter the neighbour %d of %d: ", j, i);
  scanf("%d", &val);
  new_node = (struct node *) malloc(sizeof(struct node));
- new_node –> vertex = val;
- new_node –> next = NULL;
+ new_node -> vertex = val;
+ new_node -> next = NULL;
  if (Adj[i] == NULL)
  Adj[i] = new_node;
  else
- last –> next = new_node;
- last = new_node
+ last -> next = new_node;
+ last = new_node;
  }
 }
 }
@@ -60,8 +60,8 @@ int i;
  printf("\n The neighbours of node %d are:", i);
  while(ptr != NULL)
  {
- printf("\t%d", ptr –> vertex);
- ptr = ptr –> next;
+ printf("\t%d", ptr -> vertex);
+ ptr = ptr -> next;
  }
 }
 }
@@ -72,10 +72,10 @@ struct node *temp, *ptr;
 	 for(i = 0; i <= no_of_nodes; i++)
 {
  ptr = Adj[i];
- while(ptr ! = NULL)
+ while(ptr != NULL)
  {
  temp = ptr;
- ptr = ptr –> next;
+ ptr = ptr -> next;
  free(temp);
  }
  Adj[i] = NULL;
